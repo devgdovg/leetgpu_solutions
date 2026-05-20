4eef32156847625b7bcdcaa4ea358c51431bed31
dengdong <ddwinterdd@gmail.com>
2026-05-20 14:31:08 +0800

    Delete ncu launch and rep files

--- leetgpu_int4_weight_only_quantized_matmul/ncu_launch.sh
+++
@@ -1,8 +0,0 @@
-ncu --set full \
-    --kernel-name-base function \
-    --kernel-name "regex:w4a16_matmul_kernel" \
-    --launch-skip 1 \
-    --launch-count 1 \
-    -o my_triton_report \
-    --force-overwrite \
-    python solve_3.py
